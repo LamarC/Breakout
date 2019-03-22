592ce399f343d8274aaa68e19a3505720b697311
LamarC <dviann89@gmail.com>
2019-03-22 11:58:39 -0500

    The ball moves

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
 var canvas = document.getElementById("theCanvas");
 var ctx = canvas.getContext("2d");
 
-//Making a backg rectangle
-ctx.beginPath();
-ctx.rect(40, 60, 70, 70);
-ctx.fillStyle = "#F08080";
-ctx.fill();
+// //Making a backg rectangle
+// ctx.beginPath();
+// ctx.rect(40, 60, 70, 70);
+// ctx.fillStyle = "#F08080";
+// ctx.fill();
 // ctx.closePath();
+
+let x = canvas.width / 2;
+let y = canvas.height - 30;
 
 //Making a circle
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
-ctx.fillStyle = "#800080";
-ctx.fill();
-// ctx.closePath();
+function draw() {
+  //
+  let dx = 2;
+  let dy = -2;
 
-//rectangle
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "#20B2AA";
-ctx.stroke();
-ctx.closePath();
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
+  ctx.arc(x, y, 20, 0, Math.PI * 2);
+  ctx.fillStyle = "#800080";
+  ctx.fill();
+  ctx.closePath();
+  x += dx;
+  y += dy;
+}
+
+draw();
+
+setInterval(draw, 10);
+
+// //rectangle
+// ctx.beginPath();
+// ctx.rect(160, 10, 100, 40);
+// ctx.strokeStyle = "#20B2AA";
+// ctx.stroke();
+// ctx.closePath();
