d71c566c0843738f9129316ec976beabf341bd37
LamarC <dviann89@gmail.com>
2019-03-22 12:19:31 -0500

    It's  bouncinggit add -A

--- index.js
+++ index.js
@@ -10,26 +10,38 @@ var ctx = canvas.getContext("2d");
 
 let x = canvas.width / 2;
 let y = canvas.height - 30;
+let dx = 2;
+let dy = -2;
+const ballRadius = 10;
 
-//Making a circle
-function draw() {
-  //
-  let dx = 2;
-  let dy = -2;
-
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+function drawBall() {
   ctx.beginPath();
-  ctx.arc(x, y, 20, 0, Math.PI * 2);
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
   ctx.fillStyle = "#800080";
   ctx.fill();
   ctx.closePath();
-  x += dx;
-  y += dy;
 }
 
-draw();
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+  x += dx;
+  y += dy;
 
+  //Esto que la direccion se invierta en cualquiera de los casos. Lo mantiene "dentro"
+  //De arriba a abajo
+  if(y + dy > canvas.height || y + dy < 0) {
+      dy = -dy;
+  }
+
+  //Derecha a izquierda
+  if(x + dx > canvas.width || x + dx < 0) {
+    dx = -dx;
+}
+
+}
 setInterval(draw, 10);
+
 
 // //rectangle
 // ctx.beginPath();
